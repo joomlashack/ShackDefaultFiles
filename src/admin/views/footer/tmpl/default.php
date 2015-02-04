1320c489ea7f98003e1de35ee1911dd4393318d2
Anderson Grudtner Martins <anderson@ostraining.com>
2015-02-04 10:51:45 -0200

    Initial commit, moving files from installer

---
+++ src/admin/views/footer/tmpl/default.php
@@ -0,0 +1,14 @@
+<div class="row-fluid">
+    <div id="footer" class="span12">
+        <div>
+            <a href="https://www.alledia.com">
+                <img src="../media/<?php echo $this->option; ?>/images/alledia_logo_150x43.png" />
+            </a>
+        </div>
+        <br />
+        <div>
+            OSMeta is built by&nbsp;
+            <a href="https://www.alledia.com">Alledia</a>
+        </div>
+    </div>
+</div>
