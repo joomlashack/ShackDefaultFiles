e252dd87b3d972cd5f6b4a4ded3b85e0040c6562
Bill Tomczak <bill@joomlashack.com>
2021-07-29 14:51:18 -0700

    Update file headers

--- src/admin/form/fields/base.php
+++ src/admin/form/fields/base.php
@@ -1,9 +1,24 @@
 <?php
 /**
- * @package   AllediaFreeDefaultFiles
+ * @package   ShackDefaultFiles
  * @contact   www.joomlashack.com, help@joomlashack.com
- * @copyright Copyright (C) Open Sources Training, LLC, All rights reserved
- * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ * @copyright 2015-2021 Joomlashack.com. All rights reserved
+ * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of ShackDefaultFiles.
+ *
+ * ShackDefaultFiles is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ShackDefaultFiles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShackDefaultFiles.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 use Joomla\CMS\Form\FormField;
--- src/admin/form/fields/layouts/alledia/customfooter.php
+++ src/admin/form/fields/layouts/alledia/customfooter.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * @package    AllediaFreeDefaultFiles
- * @contact    www.joomlashack.com, help@joomlashack.com
- * @copyright  2018 Open Source Training, LLC. All rights reserved
- * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * @package   ShackDefaultFiles
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright 2018-2021 Joomlashack.com. All rights reserved
+ * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
  *
- * This file is part of AllediaFreeDefaultFiles.
+ * This file is part of ShackDefaultFiles.
  *
- * AllediaFreeDefaultFiles is free software: you can redistribute it and/or modify
+ * ShackDefaultFiles is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * AllediaFreeDefaultFiles is distributed in the hope that it will be useful,
+ * ShackDefaultFiles is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with AllediaFreeDefaultFiles.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ShackDefaultFiles.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 defined('_JEXEC') or die();
